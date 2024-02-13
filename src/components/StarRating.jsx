6cdb1b15e6dfcbc3c537c446ed3667c824a8338e
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-13 14:35:15 +0430

    Add: Star Rating Component

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+function StarRating({ maxRating = 5, color = "#fcc4419", size = 48 }) {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverInOut={() => setTempRating(0)}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
+    </div>
+  );
+}
+
+export default StarRating;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+// import App from "./App.jsx";
+// import "./index.css";
+import StarRating from "./components/StarRating";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
   </React.StrictMode>,
 );
