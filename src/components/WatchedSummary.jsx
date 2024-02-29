232b948cd7e436b6a4450702bee8afdb828dca77
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-29 18:00:07 +0430

    fix: show only one digit

--- src/components/WatchedSummary.jsx
+++ src/components/WatchedSummary.jsx
@@ -16,15 +16,15 @@ function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>⭐️</span>
-          <span>{avgImdbRating}</span>
+          <span>{avgImdbRating.toFixed(1)}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>{avgUserRating}</span>
+          <span>{avgUserRating.toFixed(1)}</span>
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(1)} min</span>
         </p>
       </div>
     </div>
