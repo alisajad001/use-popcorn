415c8a5cb0d021ecb69de0741e27dab23eb18161
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-03-14 22:16:39 +0430

    Feat: Handle input focus on enter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef } from 'react';
+
 function Search({ query, setQuery }) {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Enter') {
+        setQuery('');
+        inputRef.current.focus();
+      }
+    });
+  }, []);
+
   return (
     <input
+      ref={inputRef}
       className="search"
       type="text"
       placeholder="Search movies..."
